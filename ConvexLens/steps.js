91174ee9851d3da6d54357298633e502717db8eb
avkshirasagar <45484063+avkshirasagar@users.noreply.github.com>
2020-10-29 19:53:00 +0530

    Updated position for better view

--- ConvexLens/steps.js
+++ ConvexLens/steps.js
@@ -497,7 +497,8 @@ function apparatus(aparatus) {
       ];
       clickCount += 1;
       text = textStep.slice(0, 4);
-      camera.position.set(0, 5, 8.5);
+      //camera.position.set(0, 5, 8.5);
+      camera.position.set(0, 5, 10);
       break;
     case 1:
       if (aparatus == "candle") {
